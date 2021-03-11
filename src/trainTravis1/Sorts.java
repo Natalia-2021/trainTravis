b6b8727b92aec29e10758899759dd506fcbd797a
Natalia-2021 <78405665+Natalia-2021@users.noreply.github.com>
2021-03-11 10:58:39 +0200

    train first commit

---
+++ src/trainTravis1/Sorts.java
@@ -0,0 +1,20 @@
+package trainTravis1;
+
+import java.util.Comparator;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class Sorts {
+
+	public static void main(String[] args) {
+		/*
+		Supplier<Integer> randomizer = () -> new Random().nextInt(1000);
+		Stream.generate(randomizer)
+	    .limit(10)
+	    .sorted(Comparator.naturalOrder())
+	    .forEach(e -> System.out.println(e));
+	*/System.out.println("public class Sorts");
+	}
+
+}
